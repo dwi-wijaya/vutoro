555fc93fed56278f4d7c453eee2245102fa93be2
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-06-20 10:33:25 +0700

    styling

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,34 +1,32 @@
 <template>
     <div class="todo-list max-w-lg w-full mx-auto mt-10 py-6 px-8 bg-white  rounded-xl border">
-        <h1 class="text-2xl font-bold mb-4">Vue To-Do List</h1>
+        <h1 class="text-2xl font-bold mb-6 text-center">Vue To-Do List</h1>
         <div class="flex gap-3 items-center mb-8 ">
-            <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new task"
-                class="w-full p-2 rounded-md shadow-[5px_5px_0px_0px] outline outline-1 mb-1 btn-base relative" />
-            <button @click="addTodo" class="btn"><i class="fa fa-plus"></i></button>
+            <input v-model="newTodo" @keyup.enter="addTodo" placeholder="What do you need to do?"
+                class="w-full p-2 rounded-md shadow-[5px_5px_0px_0px] outline outline-1 mb-1 btn-base relative px-3" />
+            <button @click="addTodo" class="btn bg-green-200"><i class="fa fa-plus "></i></button>
         </div>
         <div>
             <ul>
-                <li v-for="todo in incompleteTodos" :key="todo.id" class="flex items-center mb-3 gap-3">
-                    <div class="flex items-center flex-1">
-                        <input type="checkbox" v-model="todo.completed" class="mr-2 appearance-none w-4 h-4 border"
-                            @change="saveTodos" />
+                <li v-for="todo in incompleteTodos" :key="todo.id" class="flex items-center mb-4 gap-4 todo-item">
+                    <div class="flex items-center flex-1 ">
+                        <input type="checkbox" v-model="todo.completed" class="mr-2 w-4 h-4 border" @change="saveTodos" />
                         <span>{{ todo.text }}</span>
                     </div>
                     <button @click="removeTodo(todo.id)" class="">
-                        <i class="fad fa-trash text-sm"></i>
+                        <i class="fad fa-trash text-sm text-red-600"></i>
                     </button>
                 </li>
             </ul>
         </div>
-        <hr class="my-6">
         <div class="">
-            <h2 class="text-xl font-semibold mb-4 leading-4">Completed Tasks</h2>
+            <h2 class="text-xl font-semibold my-6 leading-4 text-center">Completed Tasks</h2>
             <ul>
-                <li v-for="todo in completedTodos" :key="todo.id" class="flex items-center justify-between mb-3">
+                <li v-for="todo in completedTodos" :key="todo.id" class="flex items-center justify-between mb-4 todo-item">
                     <div class="flex items-center">
-                        <input type="checkbox" v-model="todo.completed" class="mr-2 accent-red-500 w-4 h-4 border"
+                        <input type="checkbox" v-model="todo.completed" class="mr-2 accent-green-600 w-4 h-4 border"
                             @change="saveTodos" />
-                        <span class="line-through text-slate-500">{{ todo.text }}</span>
+                        <span class="line-through text-slate-600">{{ todo.text }}</span>
                     </div>
                     <button @click="removeTodo(todo.id)" class="">
                         <i class="fad fa-trash text-sm"></i>
